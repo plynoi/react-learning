b5585d96b518c9abeca3a2b388251ca787a2a362
wasin waeosri <wasin.waeosri@thomsonreuters.com>
2016-03-03 11:11:58 +0700

    finish chapter 3

--- snapterest/source/app.js
+++ snapterest/source/app.js
@@ -5,7 +5,17 @@ var ReactDom = require('react-dom');
 
 
 var ReactClass = React.createClass({
-    render: function(){
+    getInitialState: function () {
+        return {
+            isHeaderHidden: false
+        };
+    },
+    handleClick: function () {
+        this.setState({
+            isHeaderHidden: !this.state.isHeaderHidden
+        });
+    },
+    render: function () {
         /*var elementState = {
           isHidden: true  
         };
@@ -13,11 +23,17 @@ var ReactClass = React.createClass({
             return null;
         }*/
         
-        if(this.props.isHidden){
-            return null;
+        var title = 'Stateful React Component';
+
+        var headerElement = React.createElement('h1', { className: 'header', key: 'header' }, title);
+        var buttonElement = React.createElement('button', { className: 'btn btn-default', onClick: this.handleClick, key: 'button' }, 'Toggle header');
+
+        if (this.state.isHeaderHidden) {
+            return React.createElement('div', null,[buttonElement]);
         }
-        var header = this.props.tweets.length + ' Latest Tweets';
-        return React.createElement('h1', {className: 'header'}, header);
+        //var header = this.props.tweets.length + ' Latest Tweets';
+        //return React.createElement('h1', { className: 'header' }, 'React Component');
+        return React.createElement('div', null,[buttonElement, headerElement]);
     }
 });
 
