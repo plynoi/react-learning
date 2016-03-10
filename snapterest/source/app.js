b7a9f99362cd196110da041b7d70065c9ff70644
wasin waeosri <wasin.waeosri@thomsonreuters.com>
2016-03-10 18:18:37 +0700

    coding chapter4

--- snapterest/source/app.js
+++ snapterest/source/app.js
@@ -1,43 +1,8 @@
+//Chapter 4
 var React = require('react');
 var ReactDom = require('react-dom');
+var Application = require('./components/Application.react');
 
-//Chapter 3
-
-
-var ReactClass = React.createClass({
-    getInitialState: function () {
-        return {
-            isHeaderHidden: false
-        };
-    },
-    handleClick: function () {
-        this.setState({
-            isHeaderHidden: !this.state.isHeaderHidden
-        });
-    },
-    render: function () {
-        /*var elementState = {
-          isHidden: true  
-        };
-        if(elementState.isHidden){
-            return null;
-        }*/
-        
-        var title = 'Stateful React Component';
-
-        var headerElement = React.createElement('h1', { className: 'header', key: 'header' }, title);
-        var buttonElement = React.createElement('button', { className: 'btn btn-default', onClick: this.handleClick, key: 'button' }, 'Toggle header');
-
-        if (this.state.isHeaderHidden) {
-            return React.createElement('div', null,[buttonElement]);
-        }
-        //var header = this.props.tweets.length + ' Latest Tweets';
-        //return React.createElement('h1', { className: 'header' }, 'React Component');
-        return React.createElement('div', null,[buttonElement, headerElement]);
-    }
-});
-
-var reactComponentElement = React.createElement(ReactClass);
-var reactComponent = ReactDom.render(reactComponentElement, document.getElementById('react-application'));
+ReactDOM.reander(<Application/>, document.getElementByID('react-application'));
 
 
